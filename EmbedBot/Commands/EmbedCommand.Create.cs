1f53c95f4eb5f3406213ea6882e72f5a3edcd747
Oliver Booth <me@olivr.me>
2023-07-17 20:04:12 +0100

    refactor: define create subcommand in new partial

---
+++ EmbedBot/Commands/EmbedCommand.Create.cs
@@ -0,0 +1,46 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using EmbedBot.Interactivity;
+
+namespace EmbedBot.Commands;
+
+internal sealed partial class EmbedCommand
+{
+    [SlashCommand("create", "Creates a new embed.", false)]
+    public async Task CreateEmbedAsync(InteractionContext context,
+        [Option("channel", "The channel in which to send the embed. Defaults to the current channel.")]
+        DiscordChannel? channel = null)
+    {
+        channel ??= context.Channel;
+
+        var modal = new DiscordModalBuilder(context.Client);
+        modal.WithTitle("Create Embed");
+
+        DiscordModalTextInput titleInput =
+            modal.AddInput("Title", "The title of the embed.", maxLength: 256, isRequired: true);
+        DiscordModalTextInput colorInput = modal.AddInput("Color", "e.g. #007EC6", maxLength: 7, isRequired: false);
+        DiscordModalTextInput descriptionInput = modal.AddInput("Description", "The body of the embed.",
+            isRequired: true, maxLength: 2048, inputStyle: TextInputStyle.Paragraph);
+
+        DiscordModalResponse response = await modal.Build().RespondToAsync(context.Interaction, TimeSpan.FromMinutes(5))
+            .ConfigureAwait(false);
+        if (response == DiscordModalResponse.Timeout)
+            return;
+
+        DiscordColor color = DiscordColor.CornflowerBlue;
+
+        if (!string.IsNullOrWhiteSpace(colorInput.Value))
+            color = new DiscordColor(colorInput.Value);
+
+        var embed = new DiscordEmbedBuilder();
+        embed.WithColor(color);
+        embed.WithTitle(titleInput.Value);
+        embed.WithDescription(descriptionInput.Value);
+
+        await channel.SendMessageAsync(embed).ConfigureAwait(false);
+        await context
+            .FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent($"Embed created in {channel.Mention}"))
+            .ConfigureAwait(false);
+    }
+}
--- EmbedBot/Commands/EmbedCommand.cs
+++ EmbedBot/Commands/EmbedCommand.cs
@@ -1,8 +1,5 @@
-using DSharpPlus;
-using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
 using DSharpPlus.SlashCommands.Attributes;
-using EmbedBot.Interactivity;
 
 namespace EmbedBot.Commands;
 
@@ -11,42 +8,6 @@ namespace EmbedBot.Commands;
 /// </summary>
 [SlashCommandGroup("embed", "Manges embeds.", false)]
 [SlashRequireGuild]
-internal sealed class EmbedCommand : ApplicationCommandModule
+internal sealed partial class EmbedCommand : ApplicationCommandModule
 {
-    [SlashCommand("create", "Creates a new embed.", false)]
-    public async Task CreateEmbedAsync(InteractionContext context,
-        [Option("channel", "The channel in which to send the embed. Defaults to the current channel.")]
-        DiscordChannel? channel = null)
-    {
-        channel ??= context.Channel;
-
-        var modal = new DiscordModalBuilder(context.Client);
-        modal.WithTitle("Create Embed");
-
-        DiscordModalTextInput titleInput =
-            modal.AddInput("Title", "The title of the embed.", maxLength: 256, isRequired: true);
-        DiscordModalTextInput colorInput = modal.AddInput("Color", "e.g. #007EC6", maxLength: 7, isRequired: false);
-        DiscordModalTextInput descriptionInput = modal.AddInput("Description", "The body of the embed.",
-            isRequired: true, maxLength: 2048, inputStyle: TextInputStyle.Paragraph);
-
-        DiscordModalResponse response = await modal.Build().RespondToAsync(context.Interaction, TimeSpan.FromMinutes(5))
-            .ConfigureAwait(false);
-        if (response == DiscordModalResponse.Timeout)
-            return;
-
-        DiscordColor color = DiscordColor.CornflowerBlue;
-
-        if (!string.IsNullOrWhiteSpace(colorInput.Value))
-            color = new DiscordColor(colorInput.Value);
-
-        var embed = new DiscordEmbedBuilder();
-        embed.WithColor(color);
-        embed.WithTitle(titleInput.Value);
-        embed.WithDescription(descriptionInput.Value);
-
-        await channel.SendMessageAsync(embed).ConfigureAwait(false);
-        await context
-            .FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent($"Embed created in {channel.Mention}"))
-            .ConfigureAwait(false);
-    }
 }
